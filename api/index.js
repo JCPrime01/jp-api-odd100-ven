4cb64389ca95ade0d7000139da29eac46b92a90e
JCPrime01 <272623760+JCPrime01@users.noreply.github.com>
2026-05-18 21:38:08 -0300

    chore: update shareCode to RK66FX0EHLN

--- api/index.js
+++ api/index.js
@@ -24,6 +24,6 @@
       return;
     }
 
-    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=2KCTZTV7YMF&afp=tapi100' });
+    res.writeHead(302, { Location: 'https://go.aff.esportiva.bet/glq672bl?shareCode=RK66FX0EHLN&afp=tapi100' });
     res.end();
   }
